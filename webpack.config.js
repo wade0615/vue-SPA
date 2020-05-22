05144fac6b6ae61396bec81f3b20d3ecd00e544a
Wade <wsw0615@gmail.com>
2020-05-22 20:19:46 +0800

    feat: webpack-cli

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entry: "./src/main.js", //編譯前檔案的路徑
+
+    output: {
+        path: '/Users/wade/iMarts/others/vueJest', //編譯後檔案的 絕對路徑
+        filename: "build.js" //輸出檔名
+    }
+}
