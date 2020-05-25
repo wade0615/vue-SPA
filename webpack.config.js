6df4554557e0497dd6e9a7cfba8b0374c5d024b4
Wade <wsw0615@gmail.com>
2020-05-25 14:25:44 +0800

    feat: clean up

--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    entry: "./src/main.js", //編譯前檔案的路徑
-
-    output: {
-        path: '/Users/wade/iMarts/others/vueJest', //編譯後檔案的 絕對路徑
-        filename: "build.js" //輸出檔名
-    }
-}
